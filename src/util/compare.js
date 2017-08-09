2faf779ed07fefed47629bc05363244a5b04d6fc
Tobias Cohen <me@tobiascohen.com>
2017-08-09 17:40:53 +1000

    feat: add array compare fn to allow sorting on multiple fields

--- src/util/compare.js
+++ src/util/compare.js
@@ -1,10 +1,23 @@
 // Compares two values, equivalent to comparison operator (<=>)
 export const compare = (a, b) => {
+  if (a instanceof Array && b instanceof Array) return compareArrays(a, b)
+
   if (a > b) {
-    return 1;
+    return 1
   } else if (a < b) {
-    return -1;
+    return -1
   } else {
-    return 0;
+    return 0
   }
+}
+
+const compareArrays = (a, b) => {
+  const len = Math.min(a.length, b.length)
+  for (let i = 0; i < len; i++) {
+    const elA = a[i]
+    const elB = b[i]
+    if (elA > elB) return 1
+    if (elA < elB) return -1
+  }
+  return b.length - a.length
 }
