5d879d98e9ea8facfb9e3b692bdeea9751e7f592
Tobias Cohen <me@tobiascohen.com>
2017-08-29 18:46:37 +1000

    fix: broken wizard view

--- src/view/dialog-view.js
+++ src/view/dialog-view.js
@@ -5,202 +5,185 @@ import { detectMac, detectTouch } from '../util/detect'
 import { BaseView } from './base-view'
 import { ButtonBarView } from './button-bar-view'
 
-export const DialogView = makeClass('DialogView', BaseView, (def) => {
-  def.DIALOG_ID      = 'dialog';
-  def.DIALOG_CLASS   = 'dialog';
-  def.HEADER_CLASS   = 'header';
-  def.BODY_CLASS     = 'body';
-  def.FOOTER_CLASS   = 'footer';
-  def.BLANKER_CLASS  = 'blanker';
-  def.SHOW_METHOD    = 'slide';
+const TRANSITION_END_EVENTS = 'transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd'
 
-  def._blankerCount = 0;
+export const DialogView = makeClass('DialogView', BaseView, (def) => {
+  def.DIALOG_ID      = 'dialog'
+  def.DIALOG_CLASS   = 'dialog'
+  def.HEADER_CLASS   = 'header'
+  def.BODY_CLASS     = 'body'
+  def.FOOTER_CLASS   = 'footer'
+  def.BLANKER_CLASS  = 'blanker'
+  def.SHOW_METHOD    = 'slide'
+
+  def._blankerCount = 0
 
   def.initializer(function(args) {
-    if (args == null) { args = {}; }
-    this.super();
-    this._element.attr('id', DialogView.DIALOG_ID);
-    this._element.addClass(DialogView.DIALOG_CLASS);
-    this._element.hide();
-    this._element.mousedown(e => e.stopPropagation());
-    this._element.appendTo(document.body);
-    this.load();
-    this._children.add(args.view);
-    this._subView = args.view;
-    this._subView.element().wrap(`<div class="${DialogView.BODY_CLASS}" />`);
-    this._title = args.title;
-    this._autoFocus = args.autoFocus !== false;
-    this.makeHeader();
-    this.makeFooter();
-    DialogView.showBlanker();
-    this.showDialog();
-    this.takeKeyboardFocus();
-    args.view.takeKeyboardFocus();
-  });
+    if (args == null) { args = {} }
+    this.super()
+    this._element.attr('id', DialogView.DIALOG_ID)
+    this._element.addClass(DialogView.DIALOG_CLASS)
+    this._element.hide()
+    this._element.mousedown(e => e.stopPropagation())
+    this._element.appendTo(document.body)
+    this.load()
+    this._children.add(args.view)
+    this._subView = args.view
+    this._subView.element().wrap(`<div class="${DialogView.BODY_CLASS}" />`)
+    this._title = args.title
+    this._autoFocus = args.autoFocus !== false
+    this.makeHeader()
+    this.makeFooter()
+    DialogView.showBlanker()
+    this.showDialog()
+    this.takeKeyboardFocus()
+    args.view.takeKeyboardFocus()
+  })
 
   def.initializer('withTitleView', function(title, view) {
-    return this.init({title, view});
-  });
+    return this.init({title, view})
+  })
 
   def.initializer('withTitleController', function(title, controller) {
-    this.controller(controller);
-    return this.init({title, view: controller.view()});
-  });
+    this.controller(controller)
+    return this.init({title, view: controller.view()})
+  })
 
-  def.retainedProperty('controller');
-  def.property('cancelFunction');
+  def.retainedProperty('controller')
+  def.property('cancelFunction')
 
   def.classMethod('confirm', function(title, description, confirm, cancel, fn) {
-    const view = BaseView.create();
+    const view = BaseView.create()
     let fnCalled = false
-    view.element().html(description);
+    view.element().html(description)
     view.dialogButtons = function(dialog, buttonbar) {
       buttonbar.button(confirm, function() {
-        dialog.close();
+        dialog.close()
         if (!fnCalled) {
           fn()
           fnCalled = true
         }
-      });
-      buttonbar.button(cancel, () => dialog.close());
-      if (detectMac()) { return buttonbar.reverse(); }
-    };
-    const dialog = this.createWithTitleView(title, view);
-    return view.release();
-  });
+      })
+      buttonbar.button(cancel, () => dialog.close())
+      if (detectMac()) { return buttonbar.reverse() }
+    }
+    const dialog = this.createWithTitleView(title, view)
+    return view.release()
+  })
 
   def.classMethod('showBlanker', function() {
-    this._blankerCount++;
+    this._blankerCount++
 
     // Add blanker div if it doesn't already exist
     if (jQuery(`.${this.BLANKER_CLASS}`).length < 1) {
-      const blanker = jQuery(`<div class="${DialogView.BLANKER_CLASS}"></div>`);
-      blanker.css('opacity', 0);
-      blanker.click(e => e.stopPropagation());
-      jQuery('body').append(blanker);
-      blanker.bind('touchstart touchmove touchend', e => e.preventDefault());
+      const blanker = jQuery(`<div class="${DialogView.BLANKER_CLASS}"></div>`)
+      blanker.css('opacity', 0)
+      blanker.click(e => e.stopPropagation())
+      jQuery('body').append(blanker)
+      blanker.bind('touchstart touchmove touchend', e => e.preventDefault())
 
       // Fade blanker in
-      if (jQuery.browser.webkit) {
-        return blanker.css('height', jQuery(document).height())
-            .css('width', jQuery(document).width())
-            .css('-webkit-transition', 'opacity 100ms linear')
-            .css('opacity', 0.6);
-      } else {
-        return blanker.show().animate({opacity: 0.6}, 100, 'linear');
-      }
+      blanker.css('height', jQuery(document).height())
+        .css('width', jQuery(document).width())
+        .css('transition', 'opacity 100ms linear')
+        .css('opacity', 0.6)
     } else {
       // Prevent currently visible blanker from hiding
-      return jQuery('#blanker').stop().css('opacity', 0.6);
+      return jQuery('#blanker').stop().css('opacity', 0.6)
     }
-  });
+  })
 
   def.classMethod('hideBlanker', function() {
-    this._blankerCount--;
+    this._blankerCount--
     return setTimeout(() => {
       if (DialogView._blankerCount <= 0) {
         // Get blanker div
-        const blanker = jQuery(`.${this.BLANKER_CLASS}`);
+        const blanker = jQuery(`.${this.BLANKER_CLASS}`)
         if (blanker.length > 0) {
           // Fade blanker out
-          if (jQuery.browser.webkit) {
-            return blanker.css('-webkit-transition', 'opacity 100ms linear')
-              .css('opacity', 0.0)
-              .bind('webkitTransitionEnd', function() {
-                jQuery(this).unbind('webkitTransitionEnd');
-                return blanker.remove();
-            });
-          } else {
-            return blanker.animate({opacity : 0}, 300, 'linear', () => blanker.remove());
-          }
+          blanker.css('transition', 'opacity 100ms linear')
+            .css('opacity', 0.0)
+            .on(TRANSITION_END_EVENTS, function() {
+              jQuery(this).off(TRANSITION_END_EVENTS)
+              return blanker.remove()
+          })
         }
       }
     }
-    , 50);
-  });
+    , 50)
+  })
 
   def.method('makeHeader', function() {
-    const header = BaseView.create();
-    header.load();
+    const header = BaseView.create()
+    header.load()
     header.element()
       .addClass(DialogView.HEADER_CLASS)
-      .html(`<h3>${this._title}</h3>`);
-    this.header(header);
-    return header.release();
-  });
+      .html(`<h3>${this._title}</h3>`)
+    this.header(header)
+    return header.release()
+  })
 
   def.method('makeFooter', function() {
-    const footer = ButtonBarView.create();
-    footer.element().addClass(DialogView.FOOTER_CLASS);
+    const footer = ButtonBarView.create()
+    footer.element().addClass(DialogView.FOOTER_CLASS)
     if (this._subView.dialogButtons) {
-      this._subView.dialogButtons(this, footer);
+      this._subView.dialogButtons(this, footer)
     } else {
-      footer.button('Close', this.method('close'));
-      this.cancelFunction(this.method('close'));
+      footer.button('Close', this.method('close'))
+      this.cancelFunction(this.method('close'))
     }
-    this.footer(footer);
-    return footer.release();
-  });
+    this.footer(footer)
+    return footer.release()
+  })
 
   def.method('keyDown', function(key) {
     if (key === BaseView.VK_ESCAPE) {
-      if (this._cancelFunction) { this._cancelFunction(); }
-      return true;
+      if (this._cancelFunction) { this._cancelFunction() }
+      return true
     }
-  });
+  })
 
   def.method('showDialog', function() {
-    const self = this;
+    const self = this
     if (DialogView.SHOW_METHOD === 'slide') {
-      if (jQuery.browser.webkit) {
-        this._element.css('top', 0 - this._element.height())
-            .show()
-            .css('-webkit-transition', 'top 200ms ease-in')
-            .css('top', 0)
-            .bind('webkitTransitionEnd', function() {
-              jQuery(this).css('-webkit-transition', '')
-                  .unbind('webkitTransitionEnd');
-              return self.trigger('opened');
-        });
-      } else {
-        this._element.css('top', `-${this._element.height()}px`)
-            .show()
-            .animate({top: 0}, 200, 'swing', () => self.trigger('opened'));
-      }
+      this._element.css('top', 0 - this._element.height())
+          .show()
+          .css('transition', 'top 200ms ease-in')
+          .css('top', 0)
+          .on(TRANSITION_END_EVENTS, function() {
+            jQuery(this)
+              .css('transition', '')
+              .off(TRANSITION_END_EVENTS)
+            self.trigger('opened')
+      })
     } else if (DialogView.SHOW_METHOD === 'fade') {
-      this._element.fadeIn(200);
+      this._element.fadeIn(200)
     }
 
     if (this._autoFocus && !detectTouch()) {
-      return jQuery('textarea, input, button', this._element).slice(0,1).focus();
+      return jQuery('textarea, input, button', this._element).slice(0,1).focus()
     }
-  });
+  })
 
   def.method('hideDialog', function(callback) {
     if (DialogView.SHOW_METHOD === 'slide') {
-      if (jQuery.browser.webkit) {
-        return this._element.css('-webkit-transition', 'top 200ms ease-in')
-            .css('top', 0 - this._element.height())
-            .bind('webkitTransitionEnd', function() {
-              jQuery(this).unbind('webkitTransitionEnd');
-              if (callback) { return callback(); }
-        });
-      } else {
-        return this._element.animate({top: `-${this._element.height()}px`}, 200, 'swing', function() {
-          if (callback) { return callback(); }
-        });
-      }
+      this._element.css('transition', 'top 200ms ease-in')
+          .css('top', 0 - this._element.height())
+          .on(TRANSITION_END_EVENTS, function() {
+            jQuery(this).off(TRANSITION_END_EVENTS)
+            if (callback) { return callback() }
+      })
     } else if (DialogView.SHOW_METHOD === 'fade') {
-      return this._element.fadeOut(200);
+      return this._element.fadeOut(200)
     }
-  });
+  })
 
   def.method('close', function() {
-    const self = this;
-    this._subView.returnKeyboardFocus();
-    this.returnKeyboardFocus();
-    this.trigger('closed');
-    DialogView.hideBlanker();
-    return this.hideDialog(() => self.release());
-  });
-});
+    const self = this
+    this._subView.returnKeyboardFocus()
+    this.returnKeyboardFocus()
+    this.trigger('closed')
+    DialogView.hideBlanker()
+    return this.hideDialog(() => self.release())
+  })
+})
--- src/view/wizard-view.js
+++ src/view/wizard-view.js
@@ -6,21 +6,21 @@ import { DialogView } from './dialog-view'
 
 export const WizardView = makeClass('WizardView', BaseView, (def) => {
   def.initializer(function() {
-    this.super();
+    this.super()
 
-    this._steps = [];
-    this._stepIndex = -1;
-    this._data = {};
+    this._steps = []
+    this._stepIndex = -1
+    this._data = {}
     
     // Build box for step details
-    this._stepElement = jQuery('<div style="height: 200px"></div>');
-    return this._element.append(this._stepElement);
-  });
+    this._stepElement = jQuery('<div style="height: 200px"></div>')
+    return this._element.append(this._stepElement)
+  })
   
-  def.accessor('height');
-  def.property('data');
-  def.property('steps');
-  def.property('stepIndex');
+  def.accessor('height')
+  def.property('data')
+  def.property('steps')
+  def.property('stepIndex')
   
   // Adds a step to the wizard process
   //
@@ -30,150 +30,149 @@ export const WizardView = makeClass('WizardView', BaseView, (def) => {
   //   @radioGroup   field:, options:, default:
   //   @textbox      field:, placeholder:, default:
   def.method('addStep', function(stepDefinition) {
-    this._steps.push(stepDefinition);
-    if (this.stepIndex() === -1) { return this.stepIndex(0); }
-  });
+    this._steps.push(stepDefinition)
+    if (this.stepIndex() === -1) { return this.stepIndex(0) }
+  })
   
   def.method('render', function() {
     // Load current step
     if (this._stepIndex !== -1) {
-      this.renderStep(this._steps[this._stepIndex]);
-      return this.updateButtons();
+      this.renderStep(this._steps[this._stepIndex])
+      return this.updateButtons()
     }
-  });
+  })
   
   def.method('dialogButtons', function(dialog, buttonbar) {
-    this._dialog = dialog;
-    this._buttonBar = buttonbar;
+    this._dialog = dialog
+    this._buttonBar = buttonbar
     if (this._steps.length > 1) {
-      this._backButton = buttonbar.button('&lt; Back', this.method('lastStep'));
+      this._backButton = buttonbar.button('&lt Back', this.method('lastStep'))
     }
-    this._nextButton = buttonbar.button('Next &gt;', {default: true}, this.method('nextStep'));
-    this._cancelButton = buttonbar.button('Cancel', {cancel: true}, this.method('cancel'));
-    dialog.cancelFunction(this.method('cancel'));
-    buttonbar.load();
-    return this.updateButtons();
-  });
+    this._nextButton = buttonbar.button('Next &gt', {default: true}, this.method('nextStep'))
+    this._cancelButton = buttonbar.button('Cancel', {cancel: true}, this.method('cancel'))
+    dialog.cancelFunction(this.method('cancel'))
+    buttonbar.load()
+    return this.updateButtons()
+  })
     
   def.method('renderStep', function(stepDefinition) {
-    const self = this;
-    const element = this._stepElement;
-    const data = this._data;
-    element.empty();
+    const self = this
+    const element = this._stepElement
+    const data = this._data
+    element.empty()
     const components = {
       paragraph(text) {
-        return jQuery(`<p>${text}</p>`).appendTo(element);
+        jQuery(`<p>${text}</p>`).appendTo(element)
       },
       radioGroup(options) {
-        if (!data[options.field]) { data[options.field] = options.default; }
-        const ul = self.helper().tag('ul').addClass('radio-group');
-        for (let value in options.options) {
-          var label = options.options[value];
-          (function(value) {
-            const input = jQuery(`<input type=\"radio\" name=\"${options.field}\" value=\"${value}\" />`);
-            const li = self.helper().tag('li').addClass('radio-item').append(
-              input, `<label> ${label}</label>`
-            );
-            li.click(function() {
-              data[options.field] = value;
-              jQuery('input', this).attr('checked', true);
-              jQuery(this).siblings('.radio-item-selected').removeClass('radio-item-selected');
-              return jQuery(this).addClass('radio-item-selected');
-            });
-            if (value === data[options.field]) {
-              input.attr('checked', true);
-              li.addClass('radio-item-selected');
-            }
-            return ul.append(li);
-          })(value);
-        }
-        return ul.appendTo(element);
+        if (!data[options.field]) { data[options.field] = options.default }
+        const ul = self.helper().tag('ul').addClass('radio-group')
+        Object.keys(options.options).forEach(value => {
+          var label = options.options[value]
+          const input = jQuery(`<input type=\"radio\" name=\"${options.field}\" value=\"${value}\" />`)
+          const li = self.helper().tag('li').addClass('radio-item').append(
+            jQuery('<label>').append(input, ' ', label)
+          )
+          li.click(function() {
+            data[options.field] = value
+            jQuery('input', this).attr('checked', true)
+            jQuery(this).siblings('.radio-item-selected').removeClass('radio-item-selected')
+            jQuery(this).addClass('radio-item-selected')
+          })
+          if (value === data[options.field]) {
+            input.attr('checked', true)
+            li.addClass('radio-item-selected')
+          }
+          ul.append(li)
+        })
+        ul.appendTo(element)
       },
       checkbox(options) {
-        if (!data[options.field]) { data[options.field] = options.default; }
-        const input = jQuery(`<input type=\"checkbox\" id=\"${options.field}\" />`);
+        if (!data[options.field]) { data[options.field] = options.default }
+        const input = jQuery(`<input type=\"checkbox\" id=\"${options.field}\" />`)
         input.bind('click change', function() {
-          data[options.field] = input[0].checked;
-          return undefined;
-        });
-        if (data[options.field]) { input.attr('checked', true); }
-        element.append(input, `<label for=\"${options.field}\"> ${options.title}</label>`);
-        return input;
+          data[options.field] = input[0].checked
+          return undefined
+        })
+        if (data[options.field]) { input.attr('checked', true) }
+        jQuery('<label>')
+          .append(input, ' ', options.title)
+          .appendTo(element)
       },
       textbox(options) {
-        if (!data[options.field]) { data[options.field] = options.default; }
-        const input = jQuery(`<textarea id=\"${options.field}\" />`);
+        if (!data[options.field]) { data[options.field] = options.default }
+        const input = jQuery(`<textarea id=\"${options.field}\" />`)
         input.css({
           display:  'block',
           width:    '450px',
           height:   '60px',
           margin:   '10px 0'
-        });
-        if (options.placeholder) { input.attr('placeholder', options.placeholder); }
-        input.val(data[options.field]);
+        })
+        if (options.placeholder) { input.attr('placeholder', options.placeholder) }
+        input.val(data[options.field])
         input.bind('keypress change', function() {
-          data[options.field] = input.val();
-          return undefined;
-        });
-        return input.appendTo(element);
+          data[options.field] = input.val()
+          return undefined
+        })
+        input.appendTo(element)
       }
-    };
-    return stepDefinition.call(components);
-  });
+    }
+    return stepDefinition.call(components)
+  })
   
   def.method('getHeight', function() {
-    return this._stepElement.css('height');
-  });
+    return this._stepElement.css('height')
+  })
   
   def.method('setHeight', function(value) {
-    return this._stepElement.css('height', value);
-  });
+    return this._stepElement.css('height', value)
+  })
   
   def.method('updateButtons', function() {
     if (this._buttonBar) {
-      this._buttonBar.buttonDisabled(this._backButton, this.atFirstStep());
-      return this._buttonBar.buttonTitle(this._nextButton, this.atLastStep() ? 'Finish' : 'Next &gt;');
+      this._buttonBar.buttonDisabled(this._backButton, this.atFirstStep())
+      return this._buttonBar.buttonTitle(this._nextButton, this.atLastStep() ? 'Finish' : 'Next &gt')
     }
-  });
+  })
     
   def.method('_stepIndexChanged', function(oldValue, newValue) {
     if (this._loaded) {
-      this.renderStep(this._steps[newValue]);
-      return this.updateButtons();
+      this.renderStep(this._steps[newValue])
+      return this.updateButtons()
     }
-  });
+  })
   
   def.method('atFirstStep', function() {
-    return this._stepIndex === 0;
-  });
+    return this._stepIndex === 0
+  })
   
   def.method('atLastStep', function() {
-    return this._stepIndex === (this._steps.length - 1);
-  });
+    return this._stepIndex === (this._steps.length - 1)
+  })
   
   def.method('nextStep', function() {
     if (this.atLastStep()) {
-      return this.finish();
+      return this.finish()
     } else {
-      if (this._stepIndex < (this._steps.length - 1)) { return this.stepIndex(this._stepIndex + 1); }
+      if (this._stepIndex < (this._steps.length - 1)) { return this.stepIndex(this._stepIndex + 1) }
     }
-  });
+  })
   
   def.method('lastStep', function() {
-    if (this._stepIndex > 0) { return this.stepIndex(this._stepIndex - 1); }
-  });
+    if (this._stepIndex > 0) { return this.stepIndex(this._stepIndex - 1) }
+  })
   
   def.method('finish', function() {
-    if (this._dialog) { this._dialog.close(); }
-    return this.trigger('finished', this._data);
-  });
+    if (this._dialog) { this._dialog.close() }
+    return this.trigger('finished', this._data)
+  })
   
   def.method('cancel', function() {
-    if (this._dialog) { this._dialog.close(); }
-    return this.trigger('cancelled');
-  });
+    if (this._dialog) { this._dialog.close() }
+    return this.trigger('cancelled')
+  })
 
   def.method('present', function(title) {
-    return DialogView.createWithTitleView(title, this);
-  });
-});
+    return DialogView.createWithTitleView(title, this)
+  })
+})
